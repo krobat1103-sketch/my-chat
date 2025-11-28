b4867b7b1ae7dbca95454eb236bc247a673902bc
krobat1103-sketch <krobat1103@gmail.com>
2025-11-28 14:34:06 +0900

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
 
-const ADMIN_NAME = "크로바츠";
+const ADMIN_NAME = "크로바츠입니다";
 const MAX_HISTORY = 1000;
 
 let rooms = [];
